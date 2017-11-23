9bb77f76ff038ffad62654f011842b601ec8d883
Janusz Glowiak <janusz.glowiak@pl.abb.com>
2017-11-23 14:48:30 +0100

    data extracted

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,16 @@
 import Test.Hspec
 import Lib
+import Data
 
 main :: IO ()
 main = hspec $ do
-    describe "Searching for word in line" $ do
+    describe "findWordInLine" $ do
         it "Should find word 'auto' in word 'automitve'" $ do
             (findWordInLine "auto" "automotive") `shouldBe`  True
         it "Should not find word 'auto' in word 'car'" $ do
             (findWordInLine "auto" "car") `shouldBe`  False
 
-    describe "Searching for word in grid" $ do
+    describe "findWord" $ do
         it "Should find word written from left to right" $ do
             (findWord grid "HASKELL" ) `shouldBe`  True
         it "Should find word written from right to left" $ do
@@ -29,8 +30,10 @@ main = hspec $ do
         it "Should not find word not existing" $ do
             (findWord  grid "JAVA") `shouldBe`  False
 
-    describe "Searching for words in grid" $ do
+    describe "findWords" $ do
         it "Should find word written from left to right" $ do
             (findWords grid ["HASKELL","JAVA","PERL"] ) `shouldBe`  ["HASKELL", "PERL"]
         it "Should find word written from left to right" $ do
             (findWords grid ["HASKELL","JAVA","CSHARP"] ) `shouldBe`  ["HASKELL", "CSHARP"]
+        it "Should find all languages" $ do
+                    (findWords grid languages ) `shouldBe`  languages
