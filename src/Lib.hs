cfb5721791bcfdd832b743ee041b0ecdc8b0ba5b
Janusz Glowiak <janusz.glowiak@pl.abb.com>
2017-11-23 11:39:01 +0100

    searching words horizontally forward added

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,50 @@
 module Lib
-    ( someFunc,
-      someText
+    ( formatGrid,
+      outputGrid,
+      grid,
+      languages,
+      findWordInLine,
+      findWord
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn someText
-someText = "someFunc"
+import Data.List (isInfixOf)
+
+type Grid = [String]
+
+outputGrid :: Grid -> IO ()
+outputGrid grid = putStrLn $ formatGrid grid
+
+formatGrid :: Grid -> String
+formatGrid = unlines
+
+findWord :: Grid -> String -> Bool
+findWord grid word = or $ map (findWordInLine word) grid
+
+findWordInLine :: String -> String -> Bool
+findWordInLine = isInfixOf
+
+grid = [ "__C________R___"
+       , "__SI________U__"
+       , "__HASKELL____B_"
+       , "__A__A_____S__Y"
+       , "__R___B___C____"
+       , "__PHP____H_____"
+       , "____S_LREP_____"
+       , "____I__M_Y__L__"
+       , "____L_E__T_O___"
+       , "_________HB____"
+       , "_________O_____"
+       , "________CN_____"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,14 @@ import Lib
 
 main :: IO ()
 main = hspec $ do
-    describe "How to write test" $ do
-        it "Should be able to run tests" $ do
-            someText `shouldBe`  "someFunc"
+    describe "Searching for words in line" $ do
+        it "Should find word 'auto' in word 'automitve'" $ do
+            (findWordInLine "auto" "automotive") `shouldBe`  True
+        it "Should not find word 'auto' in word 'car'" $ do
+            (findWordInLine "auto" "car") `shouldBe`  False
+
+    describe "Searching for words in grid" $ do
+        it "Should find word 'auto' in grid 'automitve','car'" $ do
+            (findWord ["automotive","car"] "auto" ) `shouldBe`  True
+        it "Should not find word 'auto' in grid 'car','train','robot'" $ do
+            (findWord  ["car","train","robot"] "auto") `shouldBe`  False
